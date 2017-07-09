e13d3085203faa83eef39a727b249fadb3e8d320
Martín Osimani <mosimanicouto@gmail.com>
2017-07-09 12:18:27 -0300

    Added base structure for src folder

--- src/presentation/app.jsx
+++ src/presentation/app.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
-import './App.css'
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <div className='App-header'>
+      <div>
+        <div>
           <h2>Welcome to Open Fermentor Frontend</h2>
         </div>
       </div>
